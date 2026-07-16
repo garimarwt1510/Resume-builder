c35becaf16884cc23a164fcd0b395370727ea40c
garimarwt1510 <garimarwt.2412@gmail.com>
2026-07-16 17:48:20 +0530

    Delete src directory

--- src/components/resume/InterestsForm.jsx
+++
@@ -1,9 +0,0 @@
-// filename: frontend/src/components/resume/InterestsForm.jsx
-import React from "react";
-import TagListForm from "./TagListForm";
-
-const InterestsForm = ({ items = [], onChange }) => (
-  <TagListForm items={items} onChange={onChange} placeholder="e.g. Rock climbing" />
-);
-
-export default InterestsForm;
--- src/hooks/useAuth.js
+++
@@ -1,3 +0,0 @@
-// filename: frontend/src/hooks/useAuth.js
-// Re-exported for convenience so components can `import { useAuth } from "../hooks/useAuth"`
-export { useAuth } from "../context/AuthContext";
